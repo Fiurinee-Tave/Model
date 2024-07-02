6a81421f269b03bbd7237e562b263507cb04f70c
gororokgimbab <ruddlaalswl@naver.com>
2024-07-02 22:30:46 +0900

    데이터셋 변경

--- api/router.py
+++ api/router.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
-from flower_recommendation2 import recommender
+from flower_recommendation_re import recommender
 
 #라우터 객체 생성
 router = APIRouter() #여러 엔드포인트(API 경로)를 그룹화하고 관리하는 데 사용

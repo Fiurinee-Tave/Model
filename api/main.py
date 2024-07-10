cc02891a4a9baf111a6c9959c92750b31cbda6ae
gororokgimbab <ruddlaalswl@naver.com>
2024-07-10 23:47:00 +0900

    aws 배포 준비

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
-from router import router as recommendation_router
+from .router import router as recommendation_router
+from mangum import Mangum
 
 app = FastAPI()
 app.include_router(recommendation_router, prefix='/api')
@@ -8,3 +9,5 @@ app.include_router(recommendation_router, prefix='/api')
 @app.get('/')
 async def read_root():
     return {"message": "Welcome to the Flower Recommendation API!"}
+
+handler = Mangum(app=app)

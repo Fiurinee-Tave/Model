0fd92e6f4c9dfdbc8e8589a95a8508d1a13ad064
gororokgimbab <ruddlaalswl@naver.com>
2024-07-12 04:01:08 +0900

    멘트생성 배포준비

---
+++ api/flower_ment.py
@@ -0,0 +1,31 @@
+import openai
+
+class Flower_Ment:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        openai.api_key = self.api_key
+    
+    def get_completion(self, prompt, model="gpt-4"):
+        messages = [{"role": "user", "content": prompt}]
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=messages,
+            temperature=0
+        )
+        return response.choices[0].message['content']
+
+    def result_ment(self, user_input, flower_name, flower_mean):
+        prompt = (
+            f"너는 지금부터 감성적인 시인이며, 대답은 마치 시인이 선물하는 것처럼 해야해. "
+            f"꽃과 꽃말과 사용자 입력에 따라서 가장 어울리는 꽃다발 선물 멘트를 한줄로 생성할거야. "
+            f"꽃은 반드시 {flower_name}이고 꽃말은 {flower_mean} 이야. 사용자 입력은 {user_input}이야. "
+            f"멘트에 꽃과 꽃말 그리고 사용자 입력이 들어가면서 사용자 입장에서의 멘트를 구체적으로 추천해줘. "
+            f"반드시 사용자 입장에서의 멘트만 간결하게 출력해야해"
+        )
+
+        return self.get_completion(prompt)
+
+    
+
+# 인스턴스 초기화
+recommender = Flower_Ment(api_key='')
--- api/router.py
+++ api/router.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from .flower_recommendation_re import recommender
 from .color_flower import recommender
+from .flower_ment import recommender
 
 #라우터 객체 생성
 router = APIRouter() #여러 엔드포인트(API 경로)를 그룹화하고 관리하는 데 사용
@@ -12,6 +13,11 @@ class RecommendRequest(BaseModel): #꽃 추천 모델
     user_month: int
 
 class FlowerColorRequest(BaseModel): #어울리는 색상 모델
+    flower_name: str
+    flower_mean: str
+
+class FlowerMentRequest(BaseModel): #멘트 생성 모델
+    user_input: str
     flower_name: str
     flower_mean: str
 
@@ -30,5 +36,13 @@ def get_flowers_color_list(request: FlowerColorRequest):
     try:
         flower_color_list = recommender.result_flower(request.flower_name, request.flower_mean)
         return {"flower_color_list": flower_color_list}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.post('/ment') #멘트 생성 모델
+def get_ment(request: FlowerColorRequest):
+    try:
+        flower_ment = recommender.result_ment(request.user_input, request.flower_name, request.flower_mean)
+        return {"flower_ment": flower_ment}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))

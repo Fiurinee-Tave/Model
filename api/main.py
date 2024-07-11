49ab005aeda7659128744b4651426a7ac3d237a8
gororokgimbab <ruddlaalswl@naver.com>
2024-07-12 00:54:26 +0900

    배포 준비 및 성능평가

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
-from .router import router as recommendation_router
+from api.router import router as recommendation_router
 
 app = FastAPI()
 app.include_router(recommendation_router, prefix='/api')
